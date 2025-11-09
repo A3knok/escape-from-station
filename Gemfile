a0d81e191c43156aed56c497df9b0cb2e192b6b3
國岡亜未 <ami@Macbook-Air-3.local>
2025-11-09 18:33:34 +0900

    add:header/static_page作成

--- Gemfile
+++ Gemfile
@@ -57,3 +57,6 @@ group :test do
   gem "capybara"
   gem "selenium-webdriver"
 end
+
+# 認証用
+gem 'devise'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users #DeviseをUserモデルに適用する
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -11,5 +12,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
 
-  root "users#top"
+  root "static_pages#index"
 end
