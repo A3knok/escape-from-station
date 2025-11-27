a56d1fbc6778da0ba7f9b46da0f840d2a03e39ec
A3knok <a3-knok113@docomo.ne.jp>
2025-11-27 17:24:47 +0900

    feat:ルート検索機能の実装

--- app/models/route.rb
+++ app/models/route.rb
@@ -5,4 +5,12 @@ class Route < ApplicationRecord
 
   validates :description, presence: true, length: { minimum: 10, maximum: 1000 }
   validates :estimated_time, presence: true, numericality: { only_integer: true, greater_than: 0 }
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["exit_id", "gate_id"]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    ["gate", "exit"]
+  end
 end
